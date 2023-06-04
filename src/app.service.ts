18709224c73acd9c9bfb1f266470112faa362278
Dikendev <diken.dev@gmail.com>
2023-06-03 23:47:15 -0300

    delete method

--- src/app.service.ts
+++ src/app.service.ts
@@ -53,4 +53,13 @@ export class AppService {
     return data.report[reportIndex];
   }
 
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+
+    return;
+  }
 }
